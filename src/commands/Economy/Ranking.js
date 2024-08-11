798c96ad8062bac5d7b359180b8c538140d3adc0
rothanak <rothanak.thoeurn@cyder.page>
2024-08-12 00:35:02 +0700

    fix

--- src/commands/Economy/Ranking.js
+++ src/commands/Economy/Ranking.js
@@ -51,12 +51,22 @@ class Ranking extends Command {
     }
 
     async run(client, ctx, args) {
-         try {
+        try {
             const usersWithBalance = await Users.find({ balance: { $gt: 0 } }).sort({ balance: -1 }).limit(10);
 
-            const description = usersWithBalance.map((user, index) => {
-                const member = ctx.guild.members.cache.get(user.userId);
-                return `${handleEmoji(index + 1)} ${member ? member.displayName : 'Unknown User'}\n ${numeral(user.balance.toLocaleString()).format()} coin ${COIN}\n`;
+            const memberPromises = usersWithBalance.map(async (user, index) => {
+                const member = await ctx.guild.members.fetch(user.userId);
+                return {
+                    emoji: handleEmoji(index + 1),
+                    displayName: member ? member.displayName : 'Unknown User',
+                    balance: numeral(user.balance).format(),
+                };
+            });
+
+            const memberData = await Promise.all(memberPromises);
+
+            const description = memberData.map(({ emoji, displayName, balance }) => {
+                return `${emoji} ${displayName}\n ${balance} coin ${COIN}\n`;
             }).join('\n');
 
             const embed = this.client.embed()
