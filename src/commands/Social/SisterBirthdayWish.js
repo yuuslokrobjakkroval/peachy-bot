21282287a0abcbcc6ed4b60afd1ef22d4f2aeaa5
Yuu <108858022+yuuslokrobjakkroval@users.noreply.github.com>
2024-08-27 23:25:17 +0700

    Update SisterBirthdayWish.js

--- src/commands/Social/SisterBirthdayWish.js
+++ src/commands/Social/SisterBirthdayWish.js
@@ -45,7 +45,7 @@ module.exports = class SisterBirthdayWish extends Command {
         const sisterMention = ctx.isInteraction
                 ? ctx.interaction.options.getUser('user')
                 : ctx.message.mentions.users.first() || ctx.guild.members.cache.get(args[0]); // Gets the mentioned user
-        const messageArg = ctx.options.getString('message'); // Gets the optional message argument
+        const messageArg = ctx?.options?.getString('message'); // Gets the optional message argument
 
         const fromKeo = messageArg && messageArg.toLowerCase().includes('from keo');
 
