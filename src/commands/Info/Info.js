60967123e33a7f14f6973f750ccf54069bfb3fb2
Yuu <108858022+yuuslokrobjakkroval@users.noreply.github.com>
2024-08-29 07:32:55 +0700

    Update Info.js

--- src/commands/Info/Info.js
+++ src/commands/Info/Info.js
@@ -5,7 +5,7 @@ class Info extends Command {
     super(client, {
       name: "info",
       description: {
-        content: "Information about the bot owner",
+        content: "Information about the bot and its features",
         examples: ["info"],
         usage: "info",
       },
@@ -30,13 +30,27 @@ class Info extends Command {
   }
 
   async run(client, ctx) {
-    const botOwnerInfo = `**Bot Owner Information**:
+    const botName = "PEACHY";
+    const botDescription = "A multi-purpose bot designed for fun and enjoyment!";
+    const botFeatures = `
+**Fun and Games**: Enjoy various games and gambling features, from slot machines to quizzes.
+**Social Interaction**: Engage with custom commands and interactive features.
+**Utility**: Manage server settings and get useful information easily.`;
+
+    const botOwnerInfo = `
+**Bot Owner Information**:
 - **Discord:** [DADDY KYUU](https://discord.com/users/966688007493140591)
 - **Facebook:** [Thoeurn Rothanak](https://www.facebook.com/thoeurnrothanak?mibextid=LQQJ4d)`;
 
+    const botInfo = `**Bot Information**:
+- **Name:** ${botName}
+- **Description:** ${botDescription}
+- **Features:** ${botFeatures}
+${botOwnerInfo}`;
+
     const embed = this.client.embed();
     return await ctx.sendMessage({
-      embeds: [embed.setColor(this.client.color.main).setDescription(botOwnerInfo)],
+      embeds: [embed.setColor(this.client.color.main).setDescription(botInfo)],
     });
   }
 }
