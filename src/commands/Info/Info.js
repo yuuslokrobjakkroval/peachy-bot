bbb8e51646f97b44c34fdc298a04868cc7ec6cae
rothanak <rothanak.thoeurn@cyder.page>
2024-09-13 22:59:10 +0700

    fix birthday

--- src/commands/Info/Info.js
+++ src/commands/Info/Info.js
@@ -39,11 +39,9 @@ module.exports = class Info extends Command {
             `${client.user.username} is your multi-purpose Discord bot designed for fun, entertainment, and utility. With ${client.user.username} features, you can go mining, try your luck at gambling, play mini-games, and much more!`
         )
         .addFields([
-          { name: '𝐎𝐖𝐍𝐄𝐑', value: `[𝐊𝐘𝐔𝐔](https://discord.com/users/966688007493140591)`, inline: false },
-          { name: '𝐁𝐀𝐁𝐘 𝐎𝐖𝐍𝐄𝐑', value: `[𝐊𝐄𝐎𝐘𝐔𝐔](https://discord.com/users/946079190971732041)`, inline: false },
           {
             name: '𝐃𝐄𝐕',
-            value: `[𝐃𝐀𝐃𝐃𝐘 𝐊𝐘𝐔𝐔](https://discord.com/users/966688007493140591)`,
+            value: `[𝐏𝐄𝐀𝐂𝐇𝐘 𝐓𝐄𝐀𝐌](https://i.imgur.com/kYXE4No.gif)`,
             inline: false,
           },
           {
@@ -53,13 +51,13 @@ module.exports = class Info extends Command {
           },
           {
             name: '𝐒𝐔𝐏𝐏𝐎𝐑𝐓𝐄𝐑',
-            value: `[𝐇𝐔𝐆𝐌𝐄](https://discord.com/users/1006597979932725320), [𝐍𝐈𝐌𝐎𝐋](https://discord.com/users/990645277033656362)`,
+            value: `[𝐇𝐔𝐆𝐌𝐄](https://discord.com/users/1006597979932725320)`,
             inline: false,
           },
         ])
         .setFooter({ text: 'Having any issues, need help, or want to report bugs? Clicking the link below' });
     const row = new ActionRowBuilder().addComponents(
-        new ButtonBuilder().setCustomId('support-link').setLabel('Click for Support').setStyle(1),
+        new ButtonBuilder().setLabel('Click for support').setStyle(5).setURL(client.config.links.support),
         new ButtonBuilder().setLabel('Invite Me!').setStyle(5).setURL(client.config.links.invite),
         new ButtonBuilder().setLabel('Vote for Me').setStyle(5).setURL(client.config.links.vote)
     );
