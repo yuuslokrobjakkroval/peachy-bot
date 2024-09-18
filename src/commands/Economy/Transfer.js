0a220875deccc82045c89f68079ff7b5de512570
rothanak <rothanak.thoeurn@cyder.page>
2024-09-18 15:02:49 +0700

    first commit

--- src/commands/Economy/Transfer.js
+++ src/commands/Economy/Transfer.js
@@ -5,7 +5,7 @@ const gif = require("../../utils/Gif");
 const numeral = require("numeral");
 const { getCollectionButton, ButtonStyle, twoButton, labelButton } = require('../../functions/function');
 
-class Transfer extends Command {
+module.exports = class Transfer extends Command {
     constructor(client) {
         super(client, {
             name: "transfer",
@@ -29,30 +29,35 @@ class Transfer extends Command {
     }
 
     async run(client, ctx, args) {
-        const userId = args[0].replace(/[<@!>]/g, '');
+        const targetUser = ctx.isInteraction
+            ? ctx.interaction.options.getUser('user') || ctx.author // Default to the author if no user is provided
+            : ctx.message.mentions.members.first() || ctx.guild.members.cache.get(args[0]) || ctx.member;
         let amount = args[1] ? parseInt(args[1], 10) : 1;
+
+        // Prevent transferring to yourself
+        if (ctx.author.id === targetUser.id) {
+            return await client.utils.sendErrorMessage(client, ctx, 'You cannot transfer coins to yourself.');
+        }
 
         if (args[1] !== 'all') {
             if (isNaN(amount) || amount <= 0) {
-                return ctx.channel.send('Please mention a valid user and amount.');
+                return await client.utils.sendErrorMessage(client, ctx, 'Please mention a valid user and amount.');
             }
         }
 
-        const targetUser = await client.users.fetch(userId);
-
         if (!targetUser) {
-            return ctx.channel.send('Please mention a valid user.');
+            return await client.utils.sendErrorMessage(client, ctx, 'Please mention a valid user.');
         }
 
-        let user = await Users.findOne({ userId: ctx.author.id });
-        let target = await Users.findOne({ userId: targetUser.id });
+        let user = await Users.findOne({userId: ctx.author.id});
+        let target = await Users.findOne({userId: targetUser.id});
 
         if (!user) {
-            return ctx.channel.send('Your balance record does not exist.');
+            return await client.utils.sendErrorMessage(client, ctx, 'Your balance record does not exist.');
         }
 
         if (!target) {
-            target = new Users({ userId: targetUser.id, balance: { coin: 0, bank: 0 } });
+            target = new Users({userId: targetUser.id, balance: {coin: 0, bank: 0}});
         }
 
         if (args[1] === 'all') {
@@ -60,7 +65,7 @@ class Transfer extends Command {
         }
 
         if (isNaN(amount) || amount <= 0 || user.balance.coin < amount) {
-            return ctx.channel.send('Invalid amount specified or insufficient balance.');
+            return await client.utils.sendErrorMessage(client, ctx, 'Invalid amount specified or insufficient balance.');
         }
 
         const targetUsername = targetUser.displayName;
@@ -77,12 +82,12 @@ class Transfer extends Command {
             .setTitle(`**Transaction - ${ctx.author.displayName}**`)
             .setDescription(`You are about to give ${formattedAmount} ${client.emoji.coin} to ${targetUsername}. Confirm?`);
 
-        const messageEmbed = await ctx.channel.send({ embeds: [embed], components: [allButtons] });
+        const messageEmbed = await ctx.channel.send({embeds: [embed], components: [allButtons]});
         const collector = getCollectionButton(messageEmbed); // 30 seconds timeout
 
         collector.on('collect', async (interaction) => {
             if (interaction.user.id !== ctx.author.id) {
-                await interaction.reply({ content: 'This button is not for you!', ephemeral: true });
+                await interaction.reply({content: 'This button is not for you!', ephemeral: true});
                 return;
             }
 
@@ -91,15 +96,15 @@ class Transfer extends Command {
                 target.balance.coin += amount;
 
                 try {
-                    await Users.findOneAndUpdate({ userId: ctx.author.id }, { balance: user.balance });
-                    await Users.findOneAndUpdate({ userId: targetUser.id }, { balance: target.balance }, { upsert: true });
+                    await Users.findOneAndUpdate({userId: ctx.author.id}, {balance: user.balance});
+                    await Users.findOneAndUpdate({userId: targetUser.id}, {balance: target.balance}, {upsert: true});
 
                     const confirmationEmbed = this.client.embed()
                         .setColor(config.color.main)
                         .setTitle(`**Transaction - ${ctx.author.displayName}**`)
                         .setDescription(`You have given ${formattedAmount} ${client.emoji.coin} to ${targetUsername}`);
 
-                    await ctx.channel.send({ embeds: [confirmationEmbed] });
+                    await ctx.channel.send({embeds: [confirmationEmbed]});
 
                     setTimeout(async () => {
                         const imageEmbed = this.client.embed()
@@ -107,7 +112,7 @@ class Transfer extends Command {
                             .setDescription(`${targetUser} wants to say ... to ${ctx.author}`)
                             .setImage(gif.thanks);
 
-                        await ctx.channel.send({ embeds: [imageEmbed] });
+                        await ctx.channel.send({embeds: [imageEmbed]});
                     }, 2000);
 
                     await messageEmbed.delete();
@@ -130,10 +135,8 @@ class Transfer extends Command {
                     .setTitle(`Transaction Timed Out`)
                     .setDescription(`You did not respond in time. The transaction has been canceled.`);
 
-                messageEmbed.edit({ embeds: [timeoutEmbed], components: [] });
+                messageEmbed.edit({embeds: [timeoutEmbed], components: []});
             }
         });
     }
 }
-
-module.exports = Transfer;
