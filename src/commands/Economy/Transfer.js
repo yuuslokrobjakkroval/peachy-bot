f22b8658ea42e3355608ce5ac1a1ce53d8d82f28
rothanak <rothanak.thoeurn@cyder.page>
2024-10-11 12:02:55 +0700

    fix

--- src/commands/Economy/Transfer.js
+++ src/commands/Economy/Transfer.js
@@ -1,18 +1,16 @@
 const { Command } = require("../../structures");
+const { getCollectionButton, ButtonStyle, twoButton, labelButton } = require('../../functions/function');
 const Users = require("../../schemas/user");
 const config = require("../../config.js");
 const gif = require("../../utils/Gif");
 const numeral = require("numeral");
-const { getCollectionButton, ButtonStyle, twoButton, labelButton } = require('../../functions/function');
-
-const pendingTransfers = new Map();
 
 module.exports = class Transfer extends Command {
     constructor(client) {
         super(client, {
             name: "transfer",
             description: {
-                content: "Transfer coin to another user.",
+                content: "Transfer coins to another user.",
                 examples: ["transfer @user 100", "transfer all"],
                 usage: "transfer <user> [amount]",
             },
@@ -32,13 +30,17 @@ module.exports = class Transfer extends Command {
 
     async run(client, ctx, args, color, emoji, language) {
         const transferMessages = language.locales.get(language.defaultLocale)?.economyMessages?.transferMessages;
+        const generalMessages = language.locales.get(language.defaultLocale)?.generalMessages;
+
+        // Fetch the target user
         const targetUser = ctx.isInteraction
             ? ctx.interaction.options.getUser('user') || ctx.author
             : ctx.message.mentions.members.first() || ctx.guild.members.cache.get(args[0]) || ctx.member;
 
+        // Validate target user and amount
         let amount = args[1] === 'all' ? 'all' : parseInt(args[1], 10);
 
-        // Prevent transferring to yourself or the bot
+        // Prevent transferring to self or bot
         if (ctx.author.id === targetUser.id) {
             return await client.utils.sendErrorMessage(client, ctx, transferMessages.selfTransfer, color);
         }
@@ -51,10 +53,11 @@ module.exports = class Transfer extends Command {
             return await client.utils.sendErrorMessage(client, ctx, transferMessages.invalidAmount, color);
         }
 
+        // Fetch user data for both sender and receiver
         const sourceUserData = await Users.findOne({ userId: ctx.author.id });
         const targetUserData = await Users.findOne({ userId: targetUser.id }) || new Users({ userId: targetUser.id, balance: { coin: 0, bank: 0 } });
 
-        // Ensure source user exists
+        // Ensure sender has a balance record
         if (!sourceUserData) {
             return await client.utils.sendErrorMessage(client, ctx, transferMessages.balanceNotExist, color);
         }
@@ -64,9 +67,9 @@ module.exports = class Transfer extends Command {
             amount = sourceUserData.balance.coin;
         }
 
-        // Validate the transfer amount
+        // Check for sufficient balance
         if (sourceUserData.balance.coin < amount) {
-            return await client.utils.sendErrorMessage(client, ctx, transferMessages.transferInsufficientFunds, color);
+            return await client.utils.sendErrorMessage(client, ctx, transferMessages.insufficientFunds, color);
         }
 
         const targetUsername = targetUser.displayName;
@@ -80,54 +83,63 @@ module.exports = class Transfer extends Command {
         // Embed for confirmation
         const embed = client.embed()
             .setColor(config.color.main)
-            .setTitle(transferMessages.transactionConfirm) // Updated title
-            .setDescription(`You are about to give ${formattedAmount} ${emoji.coin} to ${targetUsername}. Confirm?`);
+            .setTitle(transferMessages.title.replace('{{user}}', ctx.author.displayName))
+            // .setTitle(transferMessages.confirm)
+            .setDescription(transferMessages.confirm
+                .replace('{{amount}}', formattedAmount)
+                .replace('{{emoji}}',  emoji.coin)
+                .replace('{{user}}', targetUsername)
+            );
 
         const messageEmbed = await ctx.channel.send({ embeds: [embed], components: [allButtons] });
         const collector = getCollectionButton(messageEmbed);
 
         collector.on('collect', async (interaction) => {
             if (interaction.user.id !== ctx.author.id) {
-                await interaction.reply({ content: language.get('errors.not_for_you'), ephemeral: true });
-                return;
-            }
+                return await interaction.reply({ content: generalMessages.notForYou || "This action is not for you.", ephemeral: true });
+            } else {
 
-            if (interaction.customId === 'confirm_button') {
-                // Perform the transfer
-                sourceUserData.balance.coin -= amount;
-                targetUserData.balance.coin += amount;
+                if (interaction.customId === 'confirm_button') {
+                    // Perform the transfer
+                    sourceUserData.balance.coin -= amount;
+                    targetUserData.balance.coin += amount;
 
-                try {
-                    await Users.findOneAndUpdate({ userId: ctx.author.id }, { balance: sourceUserData.balance });
-                    await Users.findOneAndUpdate({ userId: targetUser.id }, { balance: targetUserData.balance }, { upsert: true });
+                    try {
+                        await Users.findOneAndUpdate({userId: ctx.author.id}, {balance: sourceUserData.balance});
+                        await Users.findOneAndUpdate({userId: targetUser.id}, {balance: targetUserData.balance}, {upsert: true});
 
-                    const confirmationEmbed = client.embed()
-                        .setColor(config.color.main)
-                        .setTitle(transferMessages.transactionSuccess) // Updated confirmation message
-                        .setDescription(`You have given ${formattedAmount} ${emoji.coin} to ${targetUsername}.`);
-
-                    await ctx.channel.send({ embeds: [confirmationEmbed] });
-
-                    // Thanks GIF message
-                    setTimeout(async () => {
-                        const imageEmbed = client.embed()
+                        const confirmationEmbed = client.embed()
                             .setColor(config.color.main)
-                            .setDescription(`${targetUser} wants to say thanks to ${ctx.author}.`)
-                            .setImage(gif.thanks);
+                            .setTitle(transferMessages.title.replace('{{user}}', ctx.author.displayName))
+                            .setDescription(transferMessages.success
+                                .replace('{{amount}}', formattedAmount)
+                                .replace('{{emoji}}', emoji.coin)
+                                .replace('{{user}}', targetUsername)
+                            );
 
-                        await ctx.channel.send({ embeds: [imageEmbed] });
-                    }, 2000);
+                        await ctx.channel.send({embeds: [confirmationEmbed]});
 
+                        // Optional: Thanks GIF message
+                        setTimeout(async () => {
+                            const imageEmbed = client.embed()
+                                .setColor(config.color.main)
+                                .setDescription(`${targetUser} wants to say thanks to ${ctx.author}.`)
+                                .setImage(gif.thanks);
+
+                            await ctx.channel.send({embeds: [imageEmbed]});
+                        }, 2000);
+
+                        await messageEmbed.delete();
+                    } catch (error) {
+                        console.error('Database update error:', error);
+                        // Use generalMessages for database error
+                        await ctx.channel.send(generalMessages.databaseUpdate);
+                    }
+                } else if (interaction.customId === 'cancel_button') {
+                    await ctx.channel.send(transferMessages.cancel);
                     await messageEmbed.delete();
-                } catch (error) {
-                    console.error('Database update error:', error);
-                    await ctx.channel.send(language.get('errors.database_update'));
                 }
-            } else if (interaction.customId === 'cancel_button') {
-                await ctx.channel.send(transferMessages.transactionCancel); // Updated cancel message
-                await messageEmbed.delete();
             }
-
             collector.stop();
         });
 
@@ -135,8 +147,8 @@ module.exports = class Transfer extends Command {
             if (collected.size === 0) {
                 const timeoutEmbed = client.embed()
                     .setColor(config.color.warning)
-                    .setTitle(transferMessages.transactionExpired)
-                    .setDescription(transferMessages.transactionTimeout);
+                    .setTitle(transferMessages.expired)
+                    .setDescription(transferMessages.timeout);
 
                 messageEmbed.edit({ embeds: [timeoutEmbed], components: [] });
             }
--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -345,11 +345,11 @@ module.exports = class Utils {
         });
     }
 
-    static async sendErrorMessage(client, ctx, args, color, time) {
+    static async sendErrorMessage(client, ctx, args, color, time, ephemeral) {
         const embed = client.embed()
             .setColor(color.red)
             .setDescription(args)
-        await ctx.sendMessage({ embeds: [embed] }).then(async msg => {
+        await ctx.sendMessage({ embeds: [embed], ephemeral }).then(async msg => {
             setTimeout(async () => await msg.delete().catch(() => {}), time ? time : 10000);
         });
     }
