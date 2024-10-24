9ea3a221e8c0e89ef1fb06847295b8bf69a6e847
rothanak <rothanak.thoeurn@cyder.page>
2024-10-24 21:21:32 +0700

    fix

--- src/client.js
+++ src/client.js
@@ -5,29 +5,31 @@ const PeachyClient = require('./structures/Client.js');
 const { GuildMembers, MessageContent, GuildVoiceStates, GuildMessages, Guilds, GuildMessageTyping, GuildMessageReactions } = GatewayIntentBits;
 
 const clientOptions = {
-  intents: [Guilds, GuildMessages, MessageContent, GuildVoiceStates, GuildMembers, GuildMessageTyping, GuildMessageReactions],
-  allowedMentions: {
-    parse: ['users', 'roles'],
-    repliedUser: false,
-  },
+    intents: [Guilds, GuildMessages, MessageContent, GuildVoiceStates, GuildMembers, GuildMessageTyping, GuildMessageReactions],
+    allowedMentions: {
+        parse: ['users', 'roles'],
+        repliedUser: false,
+    },
 };
 
 const client = new PeachyClient(clientOptions);
 
 setInterval(async () => {
-  const now = Date.now();
-  const giveaways = await GiveawaySchema.find({ endTime: { $lte: now }, ended: false });
-  for (const giveaway of giveaways) {
-      const giveawayMessage = await client.channels.cache.get(giveaway.channelId);
-      if (giveawayMessage) {
-        await client.utils.endGiveaway(client, client.color, client.emoji, giveawayMessage, giveaway.autopay);
+    const now = Date.now();
+    const giveaways = await GiveawaySchema.find({ endTime: { $lte: now }, ended: false });
+    for (const giveaway of giveaways) {
+        try {
+            const giveawayMessage = await client.channels.cache.get(giveaway.channelId);
+            if (giveawayMessage) {
+                await client.utils.endGiveaway(client, client.color, client.emoji, giveawayMessage, giveaway.autopay);
 
-        giveaway.ended = true;
-        await giveaway.save();
-      } else {
-        return;
-      }
-  }
-}, 6000);
+                giveaway.ended = true;
+                await giveaway.save();
+            }
+        } catch (err) {
+            console.error(`Error ending giveaway: ${err.message}`);
+        }
+    }
+}, 60000);
 
 client.start(config.token);
