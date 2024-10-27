423a605f41beba1691064d369070ea164b1f334b
rothanak <rothanak.thoeurn@cyder.page>
2024-10-27 14:07:37 +0700

    fix

--- src/commands/admin/Gift.js
+++ src/commands/admin/Gift.js
@@ -234,7 +234,7 @@ module.exports = class Gift extends Command {
                         } catch (error) {
                             console.error('Error deleting the success message:', error);
                         }
-                    }, 60000); // 10 seconds
+                    }, 6000); // 10 seconds
 
 
                     // Disable the button after claiming
@@ -258,7 +258,7 @@ module.exports = class Gift extends Command {
                     } catch (error) {
                         console.error('Error deleting the gift message:', error);
                     }
-                }, 60000); // 30 seconds
+                }, 6000); // 10 seconds
             });
 
         }
