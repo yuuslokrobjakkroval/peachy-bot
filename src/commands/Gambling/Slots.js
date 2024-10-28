da97a9ee27bd34d5b4263098787b8d704ee1c020
rothanak <rothanak.thoeurn@cyder.page>
2024-10-28 17:11:38 +0700

    fix

--- src/commands/Gambling/Slots.js
+++ src/commands/Gambling/Slots.js
@@ -70,60 +70,31 @@ module.exports = class Slots extends Command {
 			let rand = client.utils.getRandomNumber(1, 100);
 			let win = 0;
 
-			if (verify) {
-				if (rand <= 30) { // 30%
-					win = baseCoins;
-					rslots.push(SLOTS[1], SLOTS[1], SLOTS[1]);
-				} else if (rand <= 45) { // 15%
-					win = baseCoins * 2;
-					rslots.push(SLOTS[2], SLOTS[2], SLOTS[2]);
-				} else if (rand <= 55) { // 10%
-					win = baseCoins * 3;
-					rslots.push(SLOTS[3], SLOTS[3], SLOTS[3]);
-				} else if (rand <= 65) { // 15%
-					win = 0;
-					rslots.push(SLOTS[0], SLOTS[0], SLOTS[0]);
-				} else if (rand <= 68) { // 5%
-					win = baseCoins * 4;
-					rslots.push(SLOTS[4], SLOTS[4], SLOTS[4]);
-				} else if (rand <= 70) { // 2.5%
-					win = baseCoins * 10;
-					rslots.push(SLOTS[5], SLOTS[5], SLOTS[5]);
-				} else { // 40%
-					let slot1 = Math.floor(Math.random() * SLOTS.length);
-					let slot2 = Math.floor(Math.random() * SLOTS.length);
-					let slot3 = Math.floor(Math.random() * SLOTS.length);
-					if (slot2 === slot1) slot2 = (slot1 + Math.ceil(Math.random() * (SLOTS.length - 1))) % SLOTS.length;
-					if (slot3 === slot1 || slot3 === slot2) slot3 = (slot2 + Math.ceil(Math.random() * (SLOTS.length - 1))) % SLOTS.length;
-					rslots = [SLOTS[slot1], SLOTS[slot2], SLOTS[slot3]];
-				}
-			} else {
-				if (rand <= 26) { // 26%
-					win = baseCoins;
-					rslots.push(SLOTS[1], SLOTS[1], SLOTS[1]);
-				} else if (rand <= 36) { // 10%
-					win = baseCoins * 2;
-					rslots.push(SLOTS[2], SLOTS[2], SLOTS[2]);
-				} else if (rand <= 44) { // 8%
-					win = baseCoins * 3;
-					rslots.push(SLOTS[3], SLOTS[3], SLOTS[3]);
-				} else if (rand <= 54) { // 10%
-					win = 0;
-					rslots.push(SLOTS[0], SLOTS[0], SLOTS[0]);
-				} else if (rand <= 58) { // 4%
-					win = baseCoins * 4;
-					rslots.push(SLOTS[4], SLOTS[4], SLOTS[4]);
-				} else if (rand <= 60) { // 2%
-					win = baseCoins * 10;
-					rslots.push(SLOTS[5], SLOTS[5], SLOTS[5]);
-				} else { // 49.50%
-					let slot1 = Math.floor(Math.random() * SLOTS.length);
-					let slot2 = Math.floor(Math.random() * SLOTS.length);
-					let slot3 = Math.floor(Math.random() * SLOTS.length);
-					if (slot2 === slot1) slot2 = (slot1 + Math.ceil(Math.random() * (SLOTS.length - 1))) % SLOTS.length;
-					if (slot3 === slot1 || slot3 === slot2) slot3 = (slot2 + Math.ceil(Math.random() * (SLOTS.length - 1))) % SLOTS.length;
-					rslots = [SLOTS[slot1], SLOTS[slot2], SLOTS[slot3]];
-				}
+			if (rand <= 26) { // 26%
+				win = baseCoins;
+				rslots.push(SLOTS[1], SLOTS[1], SLOTS[1]);
+			} else if (rand <= 36) { // 10%
+				win = baseCoins * 2;
+				rslots.push(SLOTS[2], SLOTS[2], SLOTS[2]);
+			} else if (rand <= 44) { // 8%
+				win = baseCoins * 3;
+				rslots.push(SLOTS[3], SLOTS[3], SLOTS[3]);
+			} else if (rand <= 54) { // 10%
+				win = 0;
+				rslots.push(SLOTS[0], SLOTS[0], SLOTS[0]);
+			} else if (rand <= 58) { // 4%
+				win = baseCoins * 4;
+				rslots.push(SLOTS[4], SLOTS[4], SLOTS[4]);
+			} else if (rand <= 60) { // 2%
+				win = baseCoins * 10;
+				rslots.push(SLOTS[5], SLOTS[5], SLOTS[5]);
+			} else { // 49.50%
+				let slot1 = Math.floor(Math.random() * SLOTS.length);
+				let slot2 = Math.floor(Math.random() * SLOTS.length);
+				let slot3 = Math.floor(Math.random() * SLOTS.length);
+				if (slot2 === slot1) slot2 = (slot1 + Math.ceil(Math.random() * (SLOTS.length - 1))) % SLOTS.length;
+				if (slot3 === slot1 || slot3 === slot2) slot3 = (slot2 + Math.ceil(Math.random() * (SLOTS.length - 1))) % SLOTS.length;
+				rslots = [SLOTS[slot1], SLOTS[slot2], SLOTS[slot3]];
 			}
 
 			let newBalance = coin + win - baseCoins;
