21de78402d6c04d5c2e2df0ff3077b9649e0deb2
rothanak <rothanak.thoeurn@cyder.page>
2024-11-04 08:35:25 +0700

    fix coinflip

--- src/commands/Gambling/Coinflip.js
+++ src/commands/Gambling/Coinflip.js
@@ -1,6 +1,5 @@
 const { Command } = require('../../structures/index.js');
-const Users = require('../../schemas/user');
-const maxAmount = 200000;
+const maxAmount = 250000;
 const random = require('random-number-csprng');
 
 module.exports = class Coinflip extends Command {
@@ -46,84 +45,90 @@ module.exports = class Coinflip extends Command {
     async run(client, ctx, args, color, emoji, language) {
         const generalMessages = language.locales.get(language.defaultLocale)?.generalMessages;
         const coinflipMessages = language.locales.get(language.defaultLocale)?.gamblingMessages?.coinflipMessages;
-        const user = await Users.findOne({ userId: ctx.author.id }).exec();
-        const { coin, bank } = user.balance;
+        client.utils.getUser(ctx.author.id).then(user => {
+            const {coin, bank} = user.balance;
 
-        if (coin < 1) {
-            return await client.utils.sendErrorMessage(client, ctx, generalMessages.zeroBalance, color);
-        }
-
-        let amount = ctx.isInteraction ? ctx.interaction.options.data[0]?.value || 1 : args[0] || 1;
-        if (isNaN(amount) || amount <= 0 || amount.toString().includes('.') || amount.toString().includes(',')) {
-            const amountMap = { all: coin, half: Math.ceil(coin / 2) };
-            if (amount in amountMap) {
-                amount = amountMap[amount];
-            } else {
-                return await client.utils.sendErrorMessage(client, ctx, generalMessages.invalidAmount, color);
+            if (coin < 1) {
+                return client.utils.sendErrorMessage(client, ctx, generalMessages.zeroBalance, color);
             }
-        }
 
-        const baseCoins = parseInt(Math.min(amount, coin, maxAmount));
+            let amount = ctx.isInteraction ? ctx.interaction.options.data[0]?.value || 1 : args[0] || 1;
+            if (isNaN(amount) || amount <= 0 || amount.toString().includes('.') || amount.toString().includes(',')) {
+                const amountMap = {all: coin, half: Math.ceil(coin / 2)};
+                if (amount in amountMap) {
+                    amount = amountMap[amount];
+                } else {
+                    return client.utils.sendErrorMessage(client, ctx, generalMessages.invalidAmount, color);
+                }
+            }
 
-        // ===================================== > Choice < ===================================== \\
-        let choice = ctx.isInteraction ? ctx.interaction.options.getString('choice') : args[1];
-        if (!choice) {
-            return await client.utils.sendErrorMessage(client, ctx, coinflipMessages.invalidChoice, color);
-        }
-        choice = choice.toLowerCase();
-        if (choice.toLowerCase() === 'peach' || choice.toLowerCase() === 'p') choice = 'p';
-        else if (choice.toLowerCase() === 'goma' || choice.toLowerCase() === 'g') choice = 'g';
+            const baseCoins = parseInt(Math.min(amount, coin, maxAmount));
 
-        let rand = await random(0, 1);
-        let win = false;
-        if (rand === 0 && choice === 'g') win = true;
-        else if (rand === 1 && choice === 'p') win = true;
+            // ===================================== > Choice < ===================================== \\
+            let choice = ctx.isInteraction ? ctx.interaction.options.getString('choice') : args[1];
+            if (!choice) {
+                return client.utils.sendErrorMessage(client, ctx, coinflipMessages.invalidChoice, color);
+            }
+            choice = choice.toLowerCase();
+            if (choice.toLowerCase() === 'peach' || choice.toLowerCase() === 'p') choice = 'p';
+            else if (choice.toLowerCase() === 'goma' || choice.toLowerCase() === 'g') choice = 'g';
 
-        // ===================================== > Display < ===================================== \\
-        const flipEmbed = client.embed()
-            .setColor(color.main)
-            .setThumbnail(client.utils.emojiToImage(emoji.coinFlip.flip))
-            .setDescription(
-                coinflipMessages.description
-                    .replace('%{mainLeft}', emoji.mainLeft)
-                    .replace('%{mainRight}', emoji.mainRight)
-                    .replace('%{coinEmote}', emoji.coin)
-                    .replace('%{coin}', client.utils.formatNumber(baseCoins))
-                    .replace('%{choice}', choice === 'p' ? 'Peach' : 'Goma')
-            )
-            .setFooter({
-                text: generalMessages.gameInProgress.replace('%{user}', ctx.author.displayName),
-                iconURL: ctx.author.displayAvatarURL(),
-            })
+            let rand = random(0, 1);
+            let win = false;
+            if (rand === 0 && choice === 'g') win = true;
+            else if (rand === 1 && choice === 'p') win = true;
 
-        await ctx.sendDeferMessage({ embeds: [flipEmbed] });
-
-        const newBalance = win ? coin + baseCoins : coin - baseCoins;
-        await Users.updateOne({ userId: ctx.author.id }, { $set: { 'balance.coin': newBalance, 'balance.bank': bank } }).exec();
-
-        // ===================================== > Result < ===================================== \\
-        setTimeout(async function () {
-            const resultCoin = win ? baseCoins * 2 : baseCoins;
-            const resultEmbed = client.embed()
-                .setColor(win ? color.success : color.danger)
-                .setThumbnail(client.utils.emojiToImage(win ? (choice === 'p' ? emoji.coinFlip.peach : emoji.coinFlip.goma) : (choice === 'p' ? emoji.coinFlip.peach : emoji.coinFlip.goma)))
+            // ===================================== > Display < ===================================== \\
+            const flipEmbed = client.embed()
+                .setColor(color.main)
+                .setThumbnail(client.utils.emojiToImage(emoji.coinFlip.flip))
                 .setDescription(
-                    coinflipMessages.resultDescription
+                    coinflipMessages.description
                         .replace('%{mainLeft}', emoji.mainLeft)
                         .replace('%{mainRight}', emoji.mainRight)
                         .replace('%{coinEmote}', emoji.coin)
                         .replace('%{coin}', client.utils.formatNumber(baseCoins))
                         .replace('%{choice}', choice === 'p' ? 'Peach' : 'Goma')
-                        .replace('%{result}', win ? 'won' : 'lost')
-                        .replace('%{resultCoin}', client.utils.formatNumber(resultCoin))
-                        .replace('%{coinEmote}', emoji.coin)
                 )
                 .setFooter({
-                    text: generalMessages.gameOver.replace('%{user}', ctx.author.displayName),
+                    text: generalMessages.gameInProgress.replace('%{user}', ctx.author.displayName),
                     iconURL: ctx.author.displayAvatarURL(),
                 })
 
-            await ctx.editMessage({ embeds: [resultEmbed] });
-        }, 2000);
+            ctx.sendDeferMessage({embeds: [flipEmbed]});
+
+            user.balance.coin = win ? coin + baseCoins : coin - baseCoins;
+            user.balance.bank = bank;
+
+            user.save().then(() => {
+                // ===================================== > Result < ===================================== \\
+                setTimeout(async function () {
+                    const resultCoin = win ? baseCoins * 2 : baseCoins;
+                    const resultEmbed = client.embed()
+                        .setColor(win ? color.success : color.danger)
+                        .setThumbnail(client.utils.emojiToImage(win ? (choice === 'p' ? emoji.coinFlip.peach : emoji.coinFlip.goma) : (choice === 'p' ? emoji.coinFlip.peach : emoji.coinFlip.goma)))
+                        .setDescription(
+                            coinflipMessages.resultDescription
+                                .replace('%{mainLeft}', emoji.mainLeft)
+                                .replace('%{mainRight}', emoji.mainRight)
+                                .replace('%{coinEmote}', emoji.coin)
+                                .replace('%{coin}', client.utils.formatNumber(baseCoins))
+                                .replace('%{choice}', choice === 'p' ? 'Peach' : 'Goma')
+                                .replace('%{result}', win ? 'won' : 'lost')
+                                .replace('%{resultCoin}', client.utils.formatNumber(resultCoin))
+                                .replace('%{coinEmote}', emoji.coin)
+                        )
+                        .setFooter({
+                            text: generalMessages.gameOver.replace('%{user}', ctx.author.displayName),
+                            iconURL: ctx.author.displayAvatarURL(),
+                        })
+
+                    await ctx.editMessage({embeds: [resultEmbed]});
+                }, 2000);
+            })
+        }).catch(error => {
+                console.error('Error processing Peachy command:', error);
+                return client.utils.sendErrorMessage(client, ctx, generalMessages.userFetchError, color);
+            });
     }
 };
