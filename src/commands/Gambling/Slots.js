b8b416180d49b890ac2193096e144ad11995a806
kyuu <thoeurnrothanak@gmail.com>
2024-11-07 21:14:27 +0700

    fix slot

--- src/commands/Gambling/Slots.js
+++ src/commands/Gambling/Slots.js
@@ -96,25 +96,25 @@ module.exports = class Slots extends Command {
 					rslots = [SLOTS[slot1], SLOTS[slot2], SLOTS[slot3]];
 				}
 			} else {
-				if (rand <= 20) { // 15%
+				if (rand <= 25) { // 25%
 					win = baseCoins;
 					rslots.push(SLOTS[0], SLOTS[0], SLOTS[0]);
-				} else if (rand <= 27) { // 7%
+				} else if (rand <= 34) { // 9%
 					win = baseCoins * 3;
 					rslots.push(SLOTS[2], SLOTS[2], SLOTS[2]);
-				} else if (rand <= 32.5) { // 5%
+				} else if (rand <= 39) { // 5%
 					win = baseCoins * 4;
 					rslots.push(SLOTS[3], SLOTS[3], SLOTS[3]);
-				} else if (rand <= 36.5) { // 4%
+				} else if (rand <= 43) { // 4%
 					win = baseCoins * 5;
 					rslots.push(SLOTS[4], SLOTS[4], SLOTS[4]);
-				} else if (rand <= 46.5) { // 10%
+				} else if (rand <= 58) { // 15%
 					win = baseCoins * 2;
 					rslots.push(SLOTS[1], SLOTS[1], SLOTS[1]);
-				} else if (rand <= 48.5) { // 2
+				} else if (rand <= 60) { // 2%
 					win = baseCoins * 10;
 					rslots.push(SLOTS[5], SLOTS[5], SLOTS[5]);
-				} else { // 51.5% chance to lose
+				} else { // 40% chance to lose
 					// Generate a non-matching slot combination
 					let slot1 = Math.floor(Math.random() * SLOTS.length);
 					let slot2 = Math.floor(Math.random() * SLOTS.length);
