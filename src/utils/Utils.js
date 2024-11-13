51700bbe90cecc1e0e9e6d0c284479c839f471f6
Yuu <108858022+yuuslokrobjakkroval@users.noreply.github.com>
2024-11-13 19:13:09 +0700

    Update Utils.js

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -979,15 +979,36 @@ module.exports = class Utils {
         return client.embed()
             .setColor(client.color.main)
             .setThumbnail('https://i.imgur.com/jRjHmwW.gif')
-            .setDescription(`## **Heyoo <@${member.user.id}>** ${emoji.main.signature}\nYou has joined the server ${emoji.congratulation}
-            
-            > ${emoji.inviteTracker.inviteBy} 𝑰𝒏𝒗𝒊𝒕𝒆 𝑩𝒚 : <@${inviter.inviterId}>
-            > ${emoji.inviteTracker.inviteCode} 𝑰𝒏𝒗𝒊𝒕𝒆 𝑪𝒐𝒅𝒆 : **https://discord.gg/${inviter.inviteCode}**
-            > ${emoji.inviteTracker.inviteStats} 𝑰𝒏𝒗𝒊𝒕𝒆𝒅 𝑴𝒆𝒎𝒃𝒆𝒓 : ${inviter.uses} 𝑴𝒆𝒎𝒃𝒆𝒓𝒔
-            > ${emoji.inviteTracker.memberCreated} 𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑫𝒂𝒕𝒆 : ${accountCreationDate}
-            > ${emoji.inviteTracker.inviteMember} 𝑴𝒆𝒎𝒃𝒆𝒓𝒔 : ${memberCount} 𝑴𝒆𝒎𝒃𝒆𝒓𝒔
-            
-            `)
+            .setDescription(`## **Heyoo <@${member.user.id}>** ${emoji.main.signature}\nYou has joined the server ${emoji.congratulation`)
+            .addFields([
+            { 
+        
+        name: `${emoji.inviteTracker.inviteBy} 𝑰𝒏𝒗𝒊𝒕𝒆 𝑩𝒚`,
+            value: `<@${inviter.inviterId}>`,
+            inline: true
+        },
+        { 
+            name: `${emoji.inviteTracker.inviteCode} 𝑰𝒏𝒗𝒊𝒕𝒆 𝑪𝒐𝒅𝒆`,
+            value: `**https://discord.gg/${inviter.inviteCode}**`,
+            inline: true
+        },
+        { 
+            name: `${emoji.inviteTracker.inviteStats} 𝑰𝒏𝒗𝒊𝒕𝒆𝒅 𝑴𝒆𝒎𝒃𝒆𝒓`,
+            value: `${inviter.uses} 𝑴𝒆𝒎𝒃𝒆𝒓𝒔`,
+            inline: true
+        },
+        { 
+            name: `${emoji.inviteTracker.memberCreated} 𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝑫𝒂𝒕𝒆`,
+            value: `${accountCreationDate}`,
+            inline: true
+        },
+        { 
+            name: `${emoji.inviteTracker.inviteMember} 𝑴𝒆𝒎𝒃𝒆𝒓𝒔`,
+            value: `${memberCount} 𝑴𝒆𝒎𝒃𝒆𝒓𝒔`,
+            inline: true
+        }
+    ])
+      
             .setImage('https://i.imgur.com/XiZrSty.gif')
             .setFooter({
                 text: `Invite Tracker | Powered by ${client.user.displayName}`,
