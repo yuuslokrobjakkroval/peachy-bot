ad1dffb29aac37e77d36d5fbd7220b0b47756e90
kyuu <thoeurnrothanak@gmail.com>
2024-11-26 22:33:04 +0700

    fix style slot

--- src/commands/Gambling/Slots.js
+++ src/commands/Gambling/Slots.js
@@ -129,7 +129,7 @@ module.exports = class Slots extends Command {
 			const initialEmbed = client.embed()
 				.setColor(color.main)
 				.setThumbnail(ctx.author.displayAvatarURL({dynamic: true, size: 1024}))
-				.setDescription(`# **${emoji.slots.mainLeft} 𝐒𝐋𝐎𝐓𝐒 ${emoji.slots.mainRight}**\n ### ╔═ »•» 🌺 «• ═╗\n ## ** 「${emoji.slots.spin} ${emoji.slots.spin} ${emoji.slots.spin}」 **\n ### ╚═ •» 🌺 «•« ═╝\n\nYou bet **${client.utils.formatNumber(baseCoins)}** ${emoji.coin}\n`)
+				.setDescription(`# **${emoji.slots.mainLeft} 𝐒𝐋𝐎𝐓𝐒 ${emoji.slots.mainRight}**\n ### ╔══ »•» 🌺 «• ═╗\n ## ** 「${emoji.slots.spin} ${emoji.slots.spin} ${emoji.slots.spin}」 **\n ### ╚═ •» 🌺 «•« ══╝\n\nYou bet **${client.utils.formatNumber(baseCoins)}** ${emoji.coin}\n`)
 				.setFooter({
 					text: `${ctx.author.displayName}, your game is in progress!`,
 					iconURL: verify ? client.utils.emojiToImage(emojiImage.verify) : ctx.author.displayAvatarURL(),
@@ -147,7 +147,7 @@ module.exports = class Slots extends Command {
 			const spinEmbed = client.embed()
 				.setColor(color.main)
 				.setThumbnail(ctx.author.displayAvatarURL({dynamic: true, size: 1024}))
-				.setDescription(`# **${emoji.slots.mainLeft} 𝐒𝐋𝐎𝐓𝐒 ${emoji.slots.mainRight}**\n ### ╔═ »•» 🌺 «• ═╗\n ## ** 「${rslots[0]} ${emoji.slots.spin} ${emoji.slots.spin}」 **\n ### ╚═ •» 🌺 «•« ═╝\n\nYou bet **${client.utils.formatNumber(baseCoins)}** ${emoji.coin}\n`)
+				.setDescription(`# **${emoji.slots.mainLeft} 𝐒𝐋𝐎𝐓𝐒 ${emoji.slots.mainRight}**\n ### ╔══ »•» 🌺 «• ═╗\n ## ** 「${rslots[0]} ${emoji.slots.spin} ${emoji.slots.spin}」 **\n ### ╚═ •» 🌺 «•« ══╝\n\nYou bet **${client.utils.formatNumber(baseCoins)}** ${emoji.coin}\n`)
 				.setFooter({
 					text: `${ctx.author.displayName}, your game is in progress!`,
 					iconURL: verify ? client.utils.emojiToImage(emojiImage.verify) : ctx.author.displayAvatarURL(),
@@ -156,7 +156,7 @@ module.exports = class Slots extends Command {
 			const spinSecondEmbed = client.embed()
 				.setColor(color.main)
 				.setThumbnail(ctx.author.displayAvatarURL({dynamic: true, size: 1024}))
-				.setDescription(`# **${emoji.slots.mainLeft} 𝐒𝐋𝐎𝐓𝐒 ${emoji.slots.mainRight}**\n ### ╔═ »•» 🌺 «• ═╗\n ## ** 「${rslots[0]} ${emoji.slots.spin} ${rslots[2]}」 **\n ### ╚═ •» 🌺 «•« ═╝\n\nYou bet **${client.utils.formatNumber(baseCoins)}** ${emoji.coin}\n`)
+				.setDescription(`# **${emoji.slots.mainLeft} 𝐒𝐋𝐎𝐓𝐒 ${emoji.slots.mainRight}**\n ### ╔══ »•» 🌺 «• ═╗\n ## ** 「${rslots[0]} ${emoji.slots.spin} ${rslots[2]}」 **\n ### ╚═ •» 🌺 «•« ══╝\n\nYou bet **${client.utils.formatNumber(baseCoins)}** ${emoji.coin}\n`)
 				.setFooter({
 					text: `${ctx.author.displayName}, your game is in progress!`,
 					iconURL: verify ? client.utils.emojiToImage(emojiImage.verify) : ctx.author.displayAvatarURL(),
@@ -165,7 +165,7 @@ module.exports = class Slots extends Command {
 			const resultEmbed = client.embed()
 				.setColor(color.main)
 				.setThumbnail(ctx.author.displayAvatarURL({dynamic: true, size: 1024}))
-				.setDescription(`# **${emoji.slots.mainLeft} 𝐒𝐋𝐎𝐓𝐒 ${emoji.slots.mainRight}**\n ### ╔═ »•» 🌺 «• ═╗\n ## ** 「${rslots[0]} ${rslots[1]} ${rslots[2]}」 **\n ### ╚═ •» 🌺 «•« ═╝\n\nYou bet **${client.utils.formatNumber(baseCoins)}** ${emoji.coin}\n${win === 0 ? `and lost **${client.utils.formatNumber(baseCoins)}** ${emoji.coin}` : `and won **${client.utils.formatNumber(win)}** ${emoji.coin}`}`)
+				.setDescription(`# **${emoji.slots.mainLeft} 𝐒𝐋𝐎𝐓𝐒 ${emoji.slots.mainRight}**\n ### ╔══ »•» 🌺 «• ═╗\n ## ** 「${rslots[0]} ${rslots[1]} ${rslots[2]}」 **\n ### ╚═ •» 🌺 «•« ══╝\n\nYou bet **${client.utils.formatNumber(baseCoins)}** ${emoji.coin}\n${win === 0 ? `and lost **${client.utils.formatNumber(baseCoins)}** ${emoji.coin}` : `and won **${client.utils.formatNumber(win)}** ${emoji.coin}`}`)
 				.setFooter({
 					text: `${ctx.author.displayName}! your game is over.`,
 					iconURL: verify ? client.utils.emojiToImage(emojiImage.verify) : ctx.author.displayAvatarURL(),
