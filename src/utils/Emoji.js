4cfc1377340065048f187f665370284ce4de6bf3
kyuu <thoeurnrothanak@gmail.com>
2024-12-13 20:15:49 +0700

    fix emoji yuna

--- src/utils/Emoji.js
+++ src/utils/Emoji.js
@@ -89,7 +89,7 @@ module.exports = {
         cucumber: '<:CUMPROFILE:1308067665264513084>',
         cappuccino: '<:CAFEMAIN:1309842668200923228>',
         duckling: '<:KMAIN:1315309185138823279>',
-        yuna: '<:YUNAMAIN:1316793376254722171>',
+        yuna: '<:YUNAMAIN:1317117650131746816>',
     },
 
     // EXCHANGE
