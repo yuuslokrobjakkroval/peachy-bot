6b1c79db30d3886e139246056fa74f2d4269086d
kyuu <thoeurnrothanak@gmail.com>
2024-12-17 10:43:36 +0700

    fix relationship

--- src/commands/Relationship/Partner.js
+++ src/commands/Relationship/Partner.js
@@ -48,30 +48,42 @@ module.exports = class Partner extends Command {
         const generalMessages = language.locales.get(language.defaultLocale)?.generalMessages;
         const relationshipMessages = language.locales.get(language.defaultLocale)?.relationshipMessages;
         const action = ctx.isInteraction ? ctx.interaction.options.getString('action') : args[0];
-        const userToModify = ctx.isInteraction ? ctx.interaction.options.getUser('user')?.id : ctx.message.mentions.users.first()?.id || args[1];
-
-        if (!userToModify) {
+        const target = ctx.isInteraction ? ctx.interaction.options.getUser('user') : ctx.message.mentions.users.first() || args[1];
+        if (!target) {
             return client.utils.sendErrorMessage(client, ctx, relationshipMessages.error.userNotMentioned, color);
         }
 
+        if (target.id === ctx.author.id) {
+            return client.utils.sendErrorMessage(client, ctx, relationshipMessages.error.selfMentioned, color);
+        }
+
         const user = await client.utils.getUser(ctx.author.id);
-        const mention = await client.utils.getUser(userToModify);
+        const mention = await client.utils.getUser(target.id);
 
         if (!user || !mention) {
             return client.utils.sendErrorMessage(client, ctx, relationshipMessages.error.notRegistered, color);
         }
 
+        // Check for existing partners
+        if (user.relationship?.partner?.userId) {
+            return client.utils.sendErrorMessage(client, ctx, `${ctx.author.displayName}, 𝒚𝒐𝒖 𝒂𝒍𝒓𝒆𝒂𝒅𝒚 𝒉𝒂𝒗𝒆 𝒂 𝒑𝒂𝒓𝒕𝒏𝒆𝒓!`, color);
+        }
+
+        if (mention.relationship?.partner?.userId) {
+            return client.utils.sendErrorMessage(client, ctx, `${mention.username} 𝒂𝒍𝒓𝒆𝒂𝒅𝒚 𝒉𝒂𝒔 𝒂 𝒑𝒂𝒓𝒕𝒏𝒆𝒓!`, color);
+        }
+
         // Check for partner relationship
         if (action === 'add') {
-            await this.checkAndAddPartner(client, ctx, user, mention, color, emoji, language);
+            await this.checkAndAddPartner(client, ctx, user, target, mention, color, emoji, language);
         } else if (action === 'remove') {
-            await this.removePartner(client, ctx, user, mention, color, emoji, language);
+            await this.removePartner(client, ctx, user, target, mention, color, emoji, language);
         } else {
             return client.utils.sendErrorMessage(client, ctx, relationshipMessages.error.invalidAction, color);
         }
     }
 
-    async checkAndAddPartner(client, ctx, user, mention, color, emoji, language) {
+    async checkAndAddPartner(client, ctx, user, target, mention, color, emoji, language) {
         const badgeIds = { r01: 'r02', r02: 'r01' };
 
         // Check inventories
@@ -82,28 +94,22 @@ module.exports = class Partner extends Command {
             return client.utils.sendErrorMessage(client, ctx, `${ctx.author.displayName}, 𝒚𝒐𝒖 𝒅𝒐𝒏'𝒕 𝒉𝒂𝒗𝒆 𝒂 𝒓𝒊𝒏𝒈.`, color);
         }
         if (!mentionRing) {
-            return client.utils.sendErrorMessage(client, ctx, `${mention.username} 𝒅𝒐𝒆𝒔 𝒏𝒐𝒕 𝒉𝒂𝒗𝒆 𝒂 𝒓𝒊𝒏𝒈.`, color);
+            return client.utils.sendErrorMessage(client, ctx, `${target.displayName}, 𝒅𝒐𝒆𝒔 𝒏𝒐𝒕 𝒉𝒂𝒗𝒆 𝒂 𝒓𝒊𝒏𝒈.`, color);
         }
 
         // Check ring pairs
         if (badgeIds[userRing.id] !== mentionRing.id) {
-            return client.utils.sendErrorMessage(client, ctx, `${ctx.author.displayName} 𝒂𝒏𝒅 ${mention.username} 𝒎𝒖𝒔𝒕 𝒉𝒂𝒗𝒆 𝒄𝒐𝒎𝒑𝒍𝒆𝒎𝒆𝒏𝒕𝒂𝒓𝒚 (𝒓01 𝒂𝒏𝒅 𝒓02).`, color);
+            return client.utils.sendErrorMessage(client, ctx, `${ctx.author.displayName} 𝒂𝒏𝒅 ${target.displayName} 𝒎𝒖𝒔𝒕 𝒉𝒂𝒗𝒆 𝒄𝒐𝒎𝒑𝒍𝒆𝒎𝒆𝒏𝒕𝒂𝒓𝒚 *(𝒓01 𝒂𝒏𝒅 𝒓02)*.`, color);
         }
 
-        // Check for existing partners
-        if (user.partner?.userId) {
-            return client.utils.sendErrorMessage(client, ctx, `${ctx.author.displayName}, 𝒚𝒐𝒖 𝒂𝒍𝒓𝒆𝒂𝒅𝒚 𝒉𝒂𝒗𝒆 𝒂 𝒑𝒂𝒓𝒕𝒏𝒆𝒓!`, color);
-        }
-        if (mention.partner?.userId) {
-            return client.utils.sendErrorMessage(client, ctx, `${mention.username} 𝒂𝒍𝒓𝒆𝒂𝒅𝒚 𝒉𝒂𝒔 𝒂 𝒑𝒂𝒓𝒕𝒏𝒆𝒓!`, color);
-        }
+
 
         // Send confirmation embed to the mentioned user
         const confirmEmbed = client.embed()
             .setColor(color.main)
             .setTitle(`💍 Partner Request`)
             .setDescription(
-                `${ctx.author.username} is asking you to become their partner!\n` +
+                `${ctx.author.displayName} is asking you to become their partner!\n` +
                 `You have the complementary ring **(${mentionRing.id.toUpperCase()})** to match theirs.\n` +
                 `Do you accept this partnership?`
             )
@@ -117,56 +123,74 @@ module.exports = class Partner extends Command {
         const msg = await ctx.channel.send( {embeds: [confirmEmbed], components: [row] });
 
         const collector = msg.createMessageComponentCollector({
-            filter: async int => {
-                if (int.user.id === mention.userId && ['accept', 'decline'].includes(int.customId)) return true;
-                else {
-                    await int.reply({
-                        content: `This button for **${mention.username}**!`,
-                        ephemeral: true
-                    });
-                    return false;
-                }
-            },
+            filter: async int => {if (['accept', 'decline'].includes(int.customId)) return true },
             time: 60000,
         });
 
         collector.on('collect', async int => {
             if (int.customId === 'accept') {
-                // Update partner data
-                user.inventory = user.inventory.map(item => {
-                    if (item.id === userRing.id) {
-                        item.quantity -= 1;
-                    }
-                    return item;
-                }).filter(item => item.quantity > 0);
-                user.relationship.partner.userId = mention.userId;
-                user.relationship.partner.name = mention.username;
+                if(int.user.id === target.id) {
+                    // Update partner data
+                    // user.inventory = user.inventory.map(item => {
+                    //     if (item.id === userRing.id) {
+                    //         item.quantity -= 1;
+                    //     }
+                    //     return item;
+                    // }).filter(item => item.quantity > 0);
+                    user.relationship.partner.userId = target.id;
+                    user.relationship.partner.name = target.displayName;
+                    user.relationship.partner.xp = 0;
+                    user.relationship.partner.level = 1;
 
-                mention.inventory = mention.inventory.map(item => {
-                    if (item.id === mentionRing.id) {
-                        item.quantity -= 1;
-                    }
-                    return item;
-                }).filter(item => item.quantity > 0);
-                mention.relationship.partner.userId = ctx.author.id;
-                mention.relationship.partner.name = ctx.author.displayName;
+                    // mention.inventory = mention.inventory.map(item => {
+                    //     if (item.id === mentionRing.id) {
+                    //         item.quantity -= 1;
+                    //     }
+                    //     return item;
+                    // }).filter(item => item.quantity > 0);
+                    mention.relationship.partner.userId = ctx.author.id;
+                    mention.relationship.partner.name = ctx.author.displayName;
+                    mention.relationship.partner.xp = 0;
+                    mention.relationship.partner.level = 1;
 
-                await Promise.all([user.save(), mention.save()]);
+                    await Promise.all([user.save(), mention.save()]);
 
-                await msg.edit({
-                    content: `🎉 **${ctx.author.displayName}** and **${mention.username}** are now partners! 💍`,
-                    embeds: [],
-                    components: [],
-                });
+                    await msg.edit({
+                        content: `🎉 **${ctx.author.displayName}** and **${target.displayName}** are now partners! 💍`,
+                        embeds: [],
+                        components: [],
+                    });
+                    await int.deferUpdate();
+                } else {
+                    await int.reply({
+                        content: `This button for **${target.displayName}**!`,
+                        ephemeral: true
+                    });
+                }
             } else {
-                await int.customId.update({
-                    content: `❌ **${mention.username}** declined the partnership request.`,
-                    embeds: [],
-                    components: [],
-                });
-            }
+                if (int.user.id === target.id) {
+                    await msg.edit({
+                        content: `❌ **${target.displayName}** declined the partnership request.`,
+                        embeds: [],
+                        components: [],
+                    });
+                    await int.deferUpdate();
 
-            await int.deferUpdate();
+                } else if (int.user.id === ctx.author.id) {
+                    await msg.edit({
+                        content: `❌ the partnership request have declined by **${ctx.author.displayName}** .`,
+                        embeds: [],
+                        components: [],
+                    });
+                    await int.deferUpdate();
+
+                } else {
+                    await int.reply({
+                        content: `This button for ${ctx.author.displayName} and **${target.displayName}**!`,
+                        ephemeral: true
+                    });
+                }
+            }
         });
 
         collector.on('end', async () => {
