5ff991e3f89a13fa7bafabef25d5249229b999e4
kyuu <thoeurnrothanak@gmail.com>
2025-01-05 22:24:52 +0700

    fix

--- src/commands/Fun/Guess.js
+++ src/commands/Fun/Guess.js
@@ -1,14 +1,14 @@
 const Command = require('../../structures/Command.js');
-const canvafy = require("canvafy");
+const canvafy = require('canvafy');
 
-module.exports = class Post extends Command {
+module.exports = class Ship extends Command {
     constructor(client) {
         super(client, {
             name: 'guess',
             description: {
-                content: 'Guess the relationship between user.',
-                examples: ['guess @user'],
-                usage: 'guess <user>',
+                content: 'Guess the relationship between two users.',
+                examples: ['guess @user1 @user2'],
+                usage: 'guess <user1> <user2>',
             },
             category: 'fun',
             aliases: [],
@@ -22,8 +22,14 @@ module.exports = class Post extends Command {
             slashCommand: true,
             options: [
                 {
-                    name: 'user',
-                    description: 'The user to guess relationship',
+                    name: 'target',
+                    description: 'The first user to guess relationship',
+                    type: 6,
+                    required: true,
+                },
+                {
+                    name: 'partner',
+                    description: 'The second user to guess relationship',
                     type: 6,
                     required: true,
                 },
@@ -34,35 +40,55 @@ module.exports = class Post extends Command {
     async run(client, ctx, args, color, emoji, language) {
         try {
             if (ctx.isInteraction) {
-                await ctx.interaction.reply("Thinking...");
+                await ctx.interaction.reply('Thinking...');
             } else {
-                await ctx.sendDeferMessage("Thinking...");
+                await ctx.sendDeferMessage('Thinking...');
             }
 
-            const targetUser = ctx.isInteraction
-                ? ctx.interaction.options.getUser('user')
+            const target = ctx.isInteraction
+                ? ctx.interaction.options.getUser('target')
                 : ctx.message.mentions.users.first() || ctx.guild.members.cache.get(args[0]);
 
-            if (targetUser.id === ctx.author.id) {
-                ctx.isInteraction
-                    ? await ctx.interaction.editReply({ content: '', embeds: [], files: [] })
-                    : await ctx.editMessage({ content: '', embeds: [], files: [] });
-                return client.utils.sendErrorMessage(client, ctx, '𝒀𝒐𝒖 𝒄𝒂𝒏𝒏𝒐𝒕 𝒈𝒖𝒆𝒔𝒔 𝒘𝒊𝒕𝒉 𝒚𝒐𝒖𝒓𝒔𝒆𝒍𝒇.', color)
+            const partner = ctx.isInteraction
+                ? ctx.interaction.options.getUser('partner')
+                : ctx.message.mentions.users.at(1) || ctx.guild.members.cache.get(args[1]);
+
+            if (!target || !partner) {
+                return client.utils.sendErrorMessage(client, ctx, 'Please mention two valid users.', color);
+            }
+
+            if (target.id === partner.id) {
+                return client.utils.sendErrorMessage(client, ctx, 'You cannot guess a relationship with the same user.', color);
             }
 
             const ship = await new canvafy.Ship()
-                .setAvatars(ctx.author.displayAvatarURL({ forceStatic: true, extension: "png" }), targetUser.displayAvatarURL({ forceStatic: true, extension: "png" }))
-                .setBackground("image", "https://i.imgur.com/WRQ9JgJ.jpg")
-                .setBorder("#FFCFCF")
+                .setAvatars(
+                    target.displayAvatarURL({ forceStatic: true, extension: 'png' }),
+                    partner.displayAvatarURL({ forceStatic: true, extension: 'png' })
+                )
+                .setBackground('image', 'https://i.imgur.com/WRQ9JgJ.jpg')
+                .setBorder('#FFCFCF')
                 .setOverlayOpacity(0.7)
                 .build();
 
-
             ctx.isInteraction
-                ? await ctx.interaction.editReply({ content: '', embeds: [], files: [{ attachment: ship, name: `profile-${ctx.author.id}.png` }] })
-                : await ctx.editMessage({ content: '', embeds: [], files: [{ attachment: ship, name: `profile-${ctx.author.id}.png` }] });
+                ? await ctx.interaction.editReply({
+                    content: '',
+                    embeds: [],
+                    files: [{ attachment: ship, name: `ship-${target.username}-${partner.username}.png` }],
+                })
+                : await ctx.editMessage({
+                    content: '',
+                    embeds: [],
+                    files: [{ attachment: ship, name: `ship-${target.username}-${partner.username}.png` }],
+                });
         } catch (error) {
-            await client.utils.sendErrorMessage(client, ctx, 'An error occurred while generating your profile. Please try again later.')
+            await client.utils.sendErrorMessage(
+                client,
+                ctx,
+                'An error occurred while generating the ship image. Please try again later.',
+                color
+            );
             console.error(error);
         }
     }
