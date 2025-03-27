ad7aa1a7c4a2635ea67ca37fc187813d3728de89
rothanak <thoeurnrothanak@gmail.com>
2025-03-27 20:21:25 +0700

    fix

--- src/commands/Bank/Withdraw.js
+++ src/commands/Bank/Withdraw.js
@@ -1,4 +1,5 @@
 const { Command } = require("../../structures");
+const Users = require("../../schemas/user");
 const moment = require("moment/moment");
 const globalGif = require("../../utils/Gif");
 
@@ -33,63 +34,109 @@ module.exports = class Withdraw extends Command {
   }
 
   async run(client, ctx, args, color, emoji, language) {
-    const generalMessages = language.locales.get(language.defaultLocale)?.generalMessages;
-    const withdrawMessages = language.locales.get(language.defaultLocale)?.bankMessages?.withdrawMessages;
+    const generalMessages = language.locales.get(
+      language.defaultLocale
+    )?.generalMessages;
+    const withdrawMessages = language.locales.get(language.defaultLocale)
+      ?.bankMessages?.withdrawMessages;
 
     try {
       const user = await client.utils.getUser(ctx.author.id);
       if (!user) {
-        return client.utils.sendErrorMessage(client, ctx, generalMessages.userNotFound, color);
+        return client.utils.sendErrorMessage(
+          client,
+          ctx,
+          generalMessages.userNotFound,
+          color
+        );
       }
 
       if (user.work.rob) {
         const cooldownTime = 2 * 60 * 1000;
-        const isCooldownExpired = await client.utils.checkCooldown(ctx.author.id, "rob", cooldownTime);
+        const isCooldownExpired = await client.utils.checkCooldown(
+          ctx.author.id,
+          "rob",
+          cooldownTime
+        );
         if (!isCooldownExpired) {
-          const lastCooldownTimestamp = await client.utils.getCooldown(ctx.author.id, "rob");
-          const remainingTime = Math.ceil((lastCooldownTimestamp + cooldownTime - Date.now()) / 1000);
+          const lastCooldownTimestamp = await client.utils.getCooldown(
+            ctx.author.id,
+            "rob"
+          );
+          const remainingTime = Math.ceil(
+            (lastCooldownTimestamp + cooldownTime - Date.now()) / 1000
+          );
           const duration = moment.duration(remainingTime, "seconds");
           const minutes = Math.floor(duration.asMinutes());
           const seconds = Math.floor(duration.asSeconds()) % 60;
 
           const cooldownMessage = withdrawMessages.cooldown
-              .replace("%{minutes}", minutes)
-              .replace("%{seconds}", seconds);
-          const cooldownEmbed = client.embed().setColor(color.danger).setDescription(cooldownMessage);
+            .replace("%{minutes}", minutes)
+            .replace("%{seconds}", seconds);
+          const cooldownEmbed = client
+            .embed()
+            .setColor(color.danger)
+            .setDescription(cooldownMessage);
 
           return ctx.sendMessage({ embeds: [cooldownEmbed] });
         }
-      } else if (user.validation.isKlaKlouk || user.validation.isMultiTransfer) {
+      } else if (
+        user.validation.isKlaKlouk ||
+        user.validation.isMultiTransfer
+      ) {
         const activeCommand = user.validation.isKlaKlouk
-            ? "Kla Klouk"
-            : "Multiple Transfer";
-        return client.utils.sendErrorMessage(client, ctx, `You have already started the ${activeCommand} event. Please finish it before using this command.`, color);
+          ? "Kla Klouk"
+          : "Multiple Transfer";
+        return client.utils.sendErrorMessage(
+          client,
+          ctx,
+          `You have already started the ${activeCommand} event. Please finish it before using this command.`,
+          color
+        );
       } else {
         const { bank } = user.balance;
 
         if (bank < 1) {
-          return client.utils.sendErrorMessage(client, ctx, withdrawMessages.zeroBalance, color);
+          return client.utils.sendErrorMessage(
+            client,
+            ctx,
+            withdrawMessages.zeroBalance,
+            color
+          );
         }
 
         let amount = ctx.isInteraction
-            ? ctx.interaction.options.getString("amount")
-            : args[0] || 1;
+          ? ctx.interaction.options.getString("amount")
+          : args[0] || 1;
 
         if (amount.toString().startsWith("-")) {
           return ctx.sendMessage({
             embeds: [
-              client.embed().setColor(color.danger).setDescription(withdrawMessages.invalidAmount),
+              client
+                .embed()
+                .setColor(color.danger)
+                .setDescription(withdrawMessages.invalidAmount),
             ],
           });
         }
 
-        amount = client.utils.formatBalance(client, ctx, color, bank, amount, withdrawMessages.invalidAmount);
+        amount = client.utils.formatBalance(
+          client,
+          ctx,
+          color,
+          bank,
+          amount,
+          withdrawMessages.invalidAmount
+        );
         if (typeof amount === "object") return;
 
         if (isNaN(amount) || amount <= 0) {
           return ctx.sendMessage({
             embeds: [
-              client.embed().setColor(color.danger).setDescription(withdrawMessages.invalidAmount),
+              client
+                .embed()
+                .setColor(color.danger)
+                .setDescription(withdrawMessages.invalidAmount),
             ],
           });
         }
@@ -97,38 +144,57 @@ module.exports = class Withdraw extends Command {
         const baseCoins = Math.min(amount, bank);
 
         if (baseCoins > bank) {
-          return client.utils.sendErrorMessage(client, ctx, withdrawMessages.tooHigh, color);
+          return client.utils.sendErrorMessage(
+            client,
+            ctx,
+            withdrawMessages.tooHigh,
+            color
+          );
         }
 
-        // Update user balance
-        user.balance.coin += parseInt(baseCoins);
-        user.balance.bank -= parseInt(baseCoins);
-        await user.save();
+        await Users.findOneAndUpdate(
+          { userId: ctx.author.id },
+          {
+            $inc: {
+              "balance.coin": parseInt(baseCoins),
+              "balance.bank": -parseInt(baseCoins),
+            },
+          },
+          { new: true }
+        );
 
-        const embed = client.embed()
-            .setColor(color.main)
-            .setDescription(
-                generalMessages.title
-                    .replace("%{mainLeft}", emoji.mainLeft)
-                    .replace("%{title}", "WITHDRAW")
-                    .replace("%{mainRight}", emoji.mainRight) +
-                withdrawMessages.success
-                    .replace("%{amount}", client.utils.formatNumber(baseCoins))
-                    .replace("%{coinEmote}", emoji.coin)
-            )
-            .setImage(globalGif.banner.depositWithdraw)
-            .setFooter({
-              text:
-                  generalMessages.requestedBy.replace("%{username}", ctx.author.displayName) ||
-                  `Requested by ${ctx.author.displayName}`,
-              iconURL: ctx.author.displayAvatarURL(),
-            });
+        const embed = client
+          .embed()
+          .setColor(color.main)
+          .setDescription(
+            generalMessages.title
+              .replace("%{mainLeft}", emoji.mainLeft)
+              .replace("%{title}", "WITHDRAW")
+              .replace("%{mainRight}", emoji.mainRight) +
+              withdrawMessages.success
+                .replace("%{amount}", client.utils.formatNumber(baseCoins))
+                .replace("%{coinEmote}", emoji.coin)
+          )
+          .setImage(globalGif.banner.depositWithdraw)
+          .setFooter({
+            text:
+              generalMessages.requestedBy.replace(
+                "%{username}",
+                ctx.author.displayName
+              ) || `Requested by ${ctx.author.displayName}`,
+            iconURL: ctx.author.displayAvatarURL(),
+          });
 
         return ctx.sendMessage({ embeds: [embed] });
       }
     } catch (error) {
       console.error("Error processing withdraw command:", error);
-      client.utils.sendErrorMessage(client, ctx, generalMessages.userFetchError, color);
+      client.utils.sendErrorMessage(
+        client,
+        ctx,
+        generalMessages.userFetchError,
+        color
+      );
     }
   }
 };
