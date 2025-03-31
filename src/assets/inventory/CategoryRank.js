d23f8143ba0fef3b73cc8848371f8ce728e56346
rothanak <thoeurnrothanak@gmail.com>
2025-03-31 23:48:24 +0700

    fix

---
+++ src/assets/inventory/CategoryRank.js
@@ -0,0 +1,8 @@
+module.exports = {
+    common: '<:BBRONZE:1356307376470364321>',    // Common
+    uncommon: '<:BSILVER:1356307368429883524>',    // Uncommon
+    rare: '<:BGOLD:1356307360930336928>',        // Rare
+    epic: '<:BDIAMOND:1356307351531028580>',  // Epic
+    legendary: '<:BLEGENDARY:1356307293175808020>', // Legendary
+    master: '<:BMASTER:1356307286275920192>'     // Master
+};
