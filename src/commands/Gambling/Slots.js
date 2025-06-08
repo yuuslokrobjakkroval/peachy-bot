74347574d4ec4a1c07c290d920e75c2881da7b80
yuuslokrobjakkroval <inyourdream0112@gmail.com>
2025-06-08 08:28:58 +0700

    refactor: Remove special channel win logic from Slots command for simplification

--- src/commands/Gambling/Slots.js
+++ src/commands/Gambling/Slots.js
@@ -124,13 +124,9 @@ module.exports = class Slots extends Command {
         }
       );
 
-      // Decide the result of the slots
       let rslots = [];
       const rand = client.utils.getRandomNumber(1, 100);
       let win = 0;
-
-      // Check if the current channel is a special channel
-      const isSpecialChannel = SPECIAL_CHANNELS.includes(ctx.channelId);
 
       if (user.verification.isBlacklist) {
         if (rand <= 10) {
@@ -164,41 +160,6 @@ module.exports = class Slots extends Command {
               (slot2 + Math.ceil(Math.random() * (SLOTS.length - 1))) %
               SLOTS.length;
           rslots = [SLOTS[slot1], SLOTS[slot2], SLOTS[slot3]];
-        }
-      } else if (isSpecialChannel) {
-        console.log(`Slots command used by user ${ctx.author.displayName}`);
-        if (rand <= 25) {
-          win = baseCoins;
-          rslots.push(SLOTS[0], SLOTS[0], SLOTS[0]);
-        } else if (rand <= 38) {
-          win = baseCoins * 2;
-          rslots.push(SLOTS[1], SLOTS[1], SLOTS[1]);
-        } else if (rand <= 48) {
-          win = baseCoins * 3;
-          rslots.push(SLOTS[2], SLOTS[2], SLOTS[2]);
-        } else if (rand <= 56) {
-          win = baseCoins * 4;
-          rslots.push(SLOTS[3], SLOTS[3], SLOTS[3]);
-        } else if (rand <= 62) {
-          win = baseCoins * 5;
-          rslots.push(SLOTS[4], SLOTS[4], SLOTS[4]);
-        } else if (rand <= 65) {
-          win = baseCoins * 10;
-          rslots.push(SLOTS[5], SLOTS[5], SLOTS[5]);
-        } else {
-          const slot1 = Math.floor(Math.random() * SLOTS.length);
-          let slot2 = Math.floor(Math.random() * SLOTS.length);
-          let slot3 = Math.floor(Math.random() * SLOTS.length);
-          if (slot2 === slot1)
-            slot2 =
-              (slot1 + Math.ceil(Math.random() * (SLOTS.length - 1))) %
-              SLOTS.length;
-          if (slot3 === slot1 || slot3 === slot2)
-            slot3 =
-              (slot2 + Math.ceil(Math.random() * (SLOTS.length - 1))) %
-              SLOTS.length;
-          rslots = [SLOTS[slot1], SLOTS[slot2], SLOTS[slot3]];
-          win = 0;
         }
       } else {
         if (rand <= 24.5) {
