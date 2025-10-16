61aa7c12c23468a09d3b93db8b28f2c6992b5cc0
yuuslokrobjakkroval <thoeurnrothanak@gmail.com>
2025-10-17 06:50:15 +0700

    fix: Increase maximum bet amount for slots command to enhance gameplay

--- src/commands/Gambling/Slots.js
+++ src/commands/Gambling/Slots.js
@@ -2,7 +2,7 @@ const { Command } = require("../../structures");
 const Users = require("../../schemas/user");
 const globalEmoji = require("../../utils/Emoji");
 
-const maxAmount = 250000;
+const maxAmount = 300000;
 
 module.exports = class Slots extends Command {
   constructor(client) {
