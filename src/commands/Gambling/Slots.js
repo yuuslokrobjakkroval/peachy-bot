c04cd962024f761c219d514ca752a248596c907b
rothanak <thoeurnrothanak@gmail.com>
2025-10-23 18:31:05 +0700

    feat: Update user balance handling in slots command

--- src/commands/Gambling/Slots.js
+++ src/commands/Gambling/Slots.js
@@ -15,7 +15,7 @@ module.exports = class Slots extends Command {
       },
       category: "gambling",
       aliases: ["slot", "s"],
-      cooldown: 2,
+      cooldown: 6,
       args: false,
       permissions: {
         dev: false,
